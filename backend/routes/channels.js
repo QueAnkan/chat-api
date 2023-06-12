5ff769e9f8e3cdb8b4d70f973f1ebe04a5f772dc
QueAnka <annika.nylin@hotmail.com>
2023-06-12 15:49:30 +0200

    added get :id function

--- backend/routes/channels.js
+++ backend/routes/channels.js
@@ -17,6 +17,33 @@ router.get('/', async (req, res) => {
 	res.send(channels)
 })
 
+
+// Channel get:id hämta en kanal med hjälp av id
+
+router.get('/:chatid', async (req, res) => {
+
+	let chatid = Number(req.params.chatid)
+	console.log('chatid', chatid);
+
+	if(!isValidId(req.params.chatid)) {
+		res.sendStatus(400)
+		console.log('Incorrect value, chatid must be a number...');
+		return
+	}
+ 
+	
+	await db.read()
+	
+	let possibleChannel = db.data.channels.find( channel => channel.chatid === chatid)
+		if (!possibleChannel) {
+			res.sendStatus(404)
+			console.log('Channel not found');
+			return
+		}
+		res.send(possibleChannel)
+})
+
+
 // chanel post: skickar upp ny kanal med genererat id
 router.post('/', async (req, res) => {
 	
@@ -46,7 +73,7 @@ router.post('/', async (req, res) => {
 // router.put()
 
 
-
+// ta bort en befintlig kanal med hjälp av id
 router.delete('/:chatid', async (req, res) => {
 
 	if(!isValidId(req.params.chatid)){
@@ -55,17 +82,17 @@ router.delete('/:chatid', async (req, res) => {
 		return
 	}
 
-	let chatId = Number(req.params.chatid)
+	let chatid = Number(req.params.chatid)
 	await db.read()
 
-	let unwantedChannel = db.data.channels.find(channel => channel.chatid === chatId)
+	let unwantedChannel = db.data.channels.find(channel => channel.chatid === chatid)
 		
 	if (!unwantedChannel){
 		res.sendStatus(404)
 		console.log("Channel not found");
 		return
 	}
-	db.data.channels = db.data.channels.filter(channel => channel.chatid !== chatId)
+	db.data.channels = db.data.channels.filter(channel => channel.chatid !== chatid)
 	await db.write()
 	res.sendStatus(200)
 	console.log('Channel deleted');
